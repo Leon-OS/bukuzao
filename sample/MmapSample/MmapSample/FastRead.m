f44e2be37b68c943f0cdd46a9456ea6bd75a83e0
everettjf <everettjf@live.com>
2018-09-01 23:11:46 +0800

    page size

--- sample/MmapSample/MmapSample/FastRead.m
+++ sample/MmapSample/MmapSample/FastRead.m
@@ -7,32 +7,36 @@
 //
 
 #import "FastRead.h"
+#import <UIKit/UIKit.h>
 #include <unistd.h>
 #include <mach/mach.h>
 #include <sys/mman.h>
+#include <sys/utsname.h>
 
 @implementation FastRead
 
 + (void)run
 {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSLog(@"%@",[NSString stringWithUTF8String:systemInfo.machine]);
+    NSLog(@"%@",[UIDevice currentDevice].systemVersion);
+    
     long sz = sysconf(_SC_PAGESIZE);
-    NSLog(@"page size = %ld bytes (%ld kb)",sz,sz/1024);
-    
-    
+    NSLog(@"sc page size = %ld bytes (%ld kb)",sz,sz/1024);
+
     int pagesize = getpagesize();
-    NSLog(@"page size = %d bytes (%d kb)",pagesize,pagesize/1024);
-    
-    
+    NSLog(@"get page size = %d bytes (%d kb)",pagesize,pagesize/1024);
+
     vm_size_t vmpagesize = 0;
     host_page_size(mach_host_self(), &vmpagesize);
-    NSLog(@"vm page size = %lu bytes (%lu kb)",vmpagesize,vmpagesize/1024);
+    NSLog(@"host page size = %lu bytes (%lu kb)",vmpagesize,vmpagesize/1024);
     
     vm_size_t vmkern = vm_kernel_page_size;
     NSLog(@"vm kern page size = %lu bytes (%lu kb)",vmkern,vmkern/1024);
     
-    
-    NSLog(@"page mask = %lu bytes (%lu kb)",PAGE_MASK,PAGE_MASK/1024);
-    NSLog(@"page size = %lu bytes (%lu kb)",PAGE_SIZE,PAGE_SIZE/1024);
+    NSLog(@"page mask = %lu bytes (%u kb)",PAGE_MASK,PAGE_MASK/1024);
+    NSLog(@"page size = %lu bytes (%u kb)",PAGE_SIZE,PAGE_SIZE/1024);
     
 }
 
